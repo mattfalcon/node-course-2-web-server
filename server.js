c0516abfd91a9467032c5175d360acd4e3b19b43
Matt Valladarez <Matt Valladarez>
2017-06-02 23:03:31 -0500

    setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+//stores port as key value pairs for heroku deployment or 3000 as local port
+const port = process.env.PORT || 3000;
 var app = express();
 
 //boilerplate for webpages, 
@@ -87,7 +89,8 @@ app.get('/bad', (req, res) => {
     });
 });
 
+//making port dynamic from 3000 to deploy in heroku
 //call app.listen to bind application to a port on our machine
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
